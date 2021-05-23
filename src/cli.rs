dff9b68d06168c5ba621b525b186d5f40285ac7c
eatradish <sakiiily@aosc.io>
2021-05-23 14:42:34 +0800

    meta: init

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+use clap::{crate_version, App, AppSettings, Arg};
+
+pub fn build_cli() -> App<'static, 'static> {
+    App::new("meowdict")
+        .version(crate_version!())
+        .author("Mag Mell")
+        .about("Check chinese keyword from moedict.tw")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Input the keyword to use")
+                .required(true)
+                .index(1),
+        )
+}
---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use anyhow::{anyhow, Result};
+use serde_json::Value;
+use std::collections::HashMap;
+
+mod cli;
+
+fn main() -> Result<()> {
+    let app = cli::build_cli().get_matches();
+    let result = response_moedict(app.value_of("INPUT").unwrap())?;
+    for (k, v) in result {
+        println!("{}:", k);
+        for i in 0..v.len() {
+            println!("{}.{}", i + 1, v[i].join("\n"));
+        }
+    }
+
+    Ok(())
+}
+
+fn response_moedict(keyword: &str) -> Result<HashMap<String, Vec<Vec<String>>>> {
+    let response =
+        reqwest::blocking::get(format!("https://www.moedict.tw/a/{}.json", keyword))?.text()?;
+    let result = response.replace("~", "").replace("`", "");
+    if result.contains("<title>404 Not Found</title>") {
+        return Err(anyhow!("Could not find keyword: {}", keyword));
+    }
+    let json: HashMap<String, Value> = serde_json::from_str(&result)?;
+    let dict = json
+        .get("h")
+        .ok_or_else(|| anyhow!("Failed to get dict!"))?
+        .as_array()
+        .ok_or_else(|| anyhow!("dict is not array!"))?;
+
+    let mut result: HashMap<String, Vec<Vec<String>>> = HashMap::new();
+    for i in dict {
+        let dicts_item = i
+            .as_object()
+            .ok_or_else(|| anyhow!("dict item is not object!"))?
+            .get("d")
+            .ok_or_else(|| anyhow!("Cannot find d!"))?
+            .as_array()
+            .ok_or_else(|| anyhow!("d is not array!"))?;
+        let mut count: usize = 0;
+        for dict_item in dicts_item {
+            let dict_item = dict_item
+                .as_object()
+                .ok_or_else(|| anyhow!("d item is not object!"))?;
+            let t;
+            if let Some(v) = dict_item.get("type") {
+                t = v
+                    .as_str()
+                    .ok_or_else(|| anyhow!("This item is not String!"))?;
+            } else {
+                t = keyword;
+            }
+            if result.get(t).is_none() {
+                result.insert(t.to_string(), vec![Vec::new()]);
+                count = 0;
+            } else {
+                result.get_mut(t).unwrap().push(Vec::new());
+            }
+            if let Some(v) = dict_item.get("f") {
+                result.get_mut(t).unwrap()[count].push(
+                    v.as_str()
+                        .ok_or_else(|| anyhow!("This item is not String!"))?
+                        .to_string(),
+                );
+            } else if let Some(v) = dict_item.get("q") {
+                let item_list = v
+                    .as_array()
+                    .ok_or_else(|| anyhow!("This item is not arrays!"))?;
+                for j in item_list {
+                    if let Some(j) = j.as_str() {
+                        result.get_mut(t).unwrap()[count].push(j.to_string());
+                    }
+                }
+            } else if let Some(v) = dict_item.get("e") {
+                let item_list = v
+                    .as_array()
+                    .ok_or_else(|| anyhow!("This item is not arrays!"))?;
+                for j in item_list {
+                    if let Some(j) = j.as_str() {
+                        result.get_mut(t).unwrap()[count].push(j.to_string());
+                    }
+                }
+            } else if let Some(v) = dict_item.get("l") {
+                let item_list = v
+                    .as_array()
+                    .ok_or_else(|| anyhow!("This item is not arrays!"))?;
+                for j in item_list {
+                    if let Some(j) = j.as_str() {
+                        result.get_mut(t).unwrap()[count].push(j.to_string());
+                    }
+                }
+            }
+            count += 1;
+        }
+    }
+
+    Ok(result)
+}
