01334a2dc675a7139e4c9c20265e3614b95affcd
eatradish <sakiiily@aosc.io>
2021-06-01 01:34:50 +0800

    main: if no input words and use require words argument, print error

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use anyhow::{anyhow, Result};
-
+use anyhow::Result;
 pub mod api;
 mod cli;
 pub mod formatter;
@@ -10,39 +9,39 @@ use meowdict_console::MeowdictConsole;
 
 fn main() -> Result<()> {
     let app = cli::build_cli().get_matches();
-    if let Some(input) = app.values_of("INPUT") {
-        let mut input = input.into_iter().map(|x| x.into()).collect::<Vec<String>>();
-        let mut result_t2s = false;
-        if input.is_empty() {
-            return Err(anyhow!("Error: Require keyword is empty!"));
-        }
+    dbg!(&app.args);
+    if let Some(words) = app.values_of("INPUT") {
+        let mut resultt2s = false;
+        let mut words = words.into_iter().map(|x| x.into()).collect::<Vec<String>>();
         if app.occurrences_of("inputs2t") != 0 {
-            for i in 0..input.len() {
-                input[i] = opencc_convert(&input[i], "s2t")?;
+            for i in 0..words.len() {
+                words[i] = opencc_convert(&words[i], "s2t")?;
             }
         }
         if app.occurrences_of("resultt2s") != 0 {
-            result_t2s = true;
+            resultt2s = true;
         }
         if app.occurrences_of("translation") != 0 {
-            if let Err(e) = print_translation_result(&input) {
+            if let Err(e) = print_translation_result(&words) {
                 println!("{}", e);
             }
             return Ok(());
         }
-        print_result(&input, result_t2s)?;
+        print_result(&words, resultt2s)?;
+    } else if app.args.len() != 0 {
+        println!("Error: Require Keyword empty!");
     } else {
-        let mut input_s2t = false;
-        let mut result_t2s = false;
+        let mut input_s2t_mode = false;
+        let mut result_t2s_mode = false;
         if app.occurrences_of("inputs2tmode") != 0 {
-            input_s2t = true;
+            input_s2t_mode = true;
         }
         if app.occurrences_of("resultt2smode") != 0 {
-            result_t2s = true;
+            result_t2s_mode = true;
         }
         let mut console = MeowdictConsole {
-            input_s2t,
-            result_t2s,
+            input_s2t: input_s2t_mode,
+            result_t2s: result_t2s_mode,
         };
         console.create_console();
     }
