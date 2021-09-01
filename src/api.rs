59f051cf0194ee0538ef40389dc074aaeb263f3b
eatradish <sakiiily@aosc.io>
2021-09-02 03:08:36 +0800

    action: rewrite moedict api

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,7 @@
 use anyhow::{anyhow, Result};
 use indexmap::IndexMap;
 use reqwest::Client;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::collections::HashMap;
+use serde::Deserialize;
 
 pub struct MoedictItemResult {
     pub pinyin: Option<String>,
@@ -11,195 +9,52 @@ pub struct MoedictItemResult {
     pub defination: Option<IndexMap<String, Vec<Vec<String>>>>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct MoedictJson {
-    #[serde(flatten)]
-    json: HashMap<String, Value>,
+#[derive(Deserialize)]
+pub struct MoedictDefinition {
+    #[serde(rename(deserialize = "type"))]
+    pub word_type: Option<String>,
+    pub q: Option<Vec<String>>,
+    pub e: Option<Vec<String>>,
+    pub f: Option<String>,
+    pub l: Option<Vec<String>>,
 }
 
-pub async fn request_moedict(keyword: &str, client: &Client) -> Result<MoedictJson> {
+#[derive(Deserialize)]
+pub struct MoedictHeteronym {
+    pub p: Option<String>,
+    pub b: Option<String>,
+    pub d: Option<Vec<MoedictDefinition>>,
+}
+
+#[derive(Deserialize)]
+pub struct MoedictRawResult {
+    pub t: String,
+    pub translation: Option<IndexMap<String, Vec<String>>>,
+    pub h: Option<Vec<MoedictHeteronym>>,
+    #[serde(rename(deserialize = "English"))]
+    pub english: Option<String>,
+}
+
+pub async fn request_moedict(keyword: &str, client: &Client) -> Result<MoedictRawResult> {
     let response = client
         .get(format!("https://www.moedict.tw/a/{}.json", keyword))
         .send()
         .await?;
 
     match response.status().into() {
-        200 => Ok(response.json::<MoedictJson>().await?),
+        200 => {
+            let obj: MoedictRawResult = serde_json::from_str(
+                response
+                    .text()
+                    .await?
+                    .replace("`", "")
+                    .replace("~", "")
+                    .as_str(),
+            )?;
+
+            Ok(obj)
+        }
         404 => Err(anyhow!("Could not find keyword: {}", keyword)),
         _ => Err(anyhow!("Response status code: {}", response.status())),
-    }
-}
-
-fn api_get_h(json: &HashMap<String, Value>) -> Result<Vec<Value>> {
-    let h = json
-        .get("h")
-        .ok_or_else(|| anyhow!("Failed to get dict!"))?
-        .as_array()
-        .ok_or_else(|| anyhow!("dict is not array!"))?
-        .to_owned();
-
-    Ok(h)
-}
-
-fn api_get_translations(json: &HashMap<String, Value>) -> Result<IndexMap<String, Vec<String>>> {
-    let translation = json
-        .get("translation")
-        .ok_or_else(|| anyhow!("This item has no translation!"))?
-        .as_object()
-        .ok_or_else(|| anyhow!("translation is not Object!"))?;
-    let mut translation_indexmap: IndexMap<String, Vec<String>> = IndexMap::new();
-    for (lang, lang_value) in translation {
-        let lang_value = lang_value
-            .as_array()
-            .ok_or_else(|| anyhow!("lang_value is not Array!"))?;
-        let mut lang_vec = Vec::new();
-        for i in lang_value {
-            let i = i
-                .as_str()
-                .ok_or_else(|| anyhow!("lang_value item is not String!"))?;
-            lang_vec.push(i.to_string());
-        }
-        translation_indexmap.insert(lang.to_string(), lang_vec);
-    }
-
-    Ok(translation_indexmap)
-}
-
-fn api_get_pinyin(dict_val: &Value) -> Result<String, anyhow::Error> {
-    let pinyin = dict_val
-        .as_object()
-        .ok_or_else(|| anyhow!("dict item is not object!"))?
-        .get("p")
-        .ok_or_else(|| anyhow!("Caanot get p!"))?
-        .as_str()
-        .ok_or_else(|| anyhow!("p is not String!"))?
-        .to_owned();
-
-    Ok(pinyin.replace("~", "").replace("`", ""))
-}
-
-fn api_get_defination(dict_val: &Value) -> Result<IndexMap<String, Vec<Vec<String>>>> {
-    let mut defination_item = IndexMap::new();
-    let dicts_item = dict_val
-        .as_object()
-        .ok_or_else(|| anyhow!("dict item is not object!"))?
-        .get("d")
-        .ok_or_else(|| anyhow!("Cannot find d!"))?
-        .as_array()
-        .ok_or_else(|| anyhow!("d is not array!"))?;
-    let mut count: usize = 0;
-    for dict_item in dicts_item {
-        let dict_item = dict_item
-            .as_object()
-            .ok_or_else(|| anyhow!("d item is not object!"))?;
-        let t = if let Some(v) = dict_item.get("type") {
-            v.as_str()
-                .ok_or_else(|| anyhow!("This item is not String!"))?
-                .replace("~", "")
-                .replace("`", "")
-        } else {
-            "notype".to_string()
-        };
-        if defination_item.get(&t).is_none() {
-            defination_item.insert(t.to_string(), vec![Vec::new()]);
-            count = 0;
-        } else {
-            defination_item.get_mut(&t).unwrap().push(Vec::new());
-        }
-        if let Some(v) = dict_item.get("f") {
-            defination_item.get_mut(&t).unwrap()[count].push(
-                v.as_str()
-                    .ok_or_else(|| anyhow!("This item is not String!"))?
-                    .to_string()
-                    .replace("~", "")
-                    .replace("`", ""),
-            );
-        }
-        for i in &["q", "e", "l"] {
-            if let Some(v) = dict_item.get(&i.to_string()) {
-                let item_list = v
-                    .as_array()
-                    .ok_or_else(|| anyhow!("This item is not arrays!"))?;
-                for j in item_list {
-                    if let Some(j) = j.as_str() {
-                        defination_item.get_mut(&t).unwrap()[count]
-                            .push(j.to_string().replace("~", "").replace("`", ""));
-                    }
-                }
-            }
-        }
-        count += 1;
-    }
-    Ok(defination_item)
-}
-
-fn api_get_bopomofo(dict_val: &Value) -> Result<String> {
-    let bopomofo = dict_val
-        .as_object()
-        .ok_or_else(|| anyhow!("dict item is not object!"))?
-        .get("b")
-        .ok_or_else(|| anyhow!("Caanot get b!"))?
-        .as_str()
-        .ok_or_else(|| anyhow!("b is not String!"))?
-        .to_owned();
-
-    Ok(bopomofo.replace("`", "").replace("~", ""))
-}
-
-fn api_get_english(json: &HashMap<String, Value>) -> Result<String> {
-    let english = json
-        .get("English")
-        .ok_or_else(|| anyhow!("This item has no English!"))?
-        .as_str()
-        .ok_or_else(|| anyhow!("English is not String!"))?
-        .to_owned();
-
-    Ok(english)
-}
-
-impl MoedictJson {
-    pub fn get_translations(&self) -> Option<IndexMap<String, Vec<String>>> {
-        match api_get_translations(&self.json) {
-            Ok(v) => Some(v),
-            Err(_) => None,
-        }
-    }
-
-    pub fn get_moedict_item_result_vec(&self) -> Vec<MoedictItemResult> {
-        let mut moedict_item_result = Vec::new();
-        let dict = match api_get_h(&self.json) {
-            Ok(v) => Some(v),
-            Err(_) => None,
-        };
-        if let Some(dict) = dict {
-            for dict_val in dict {
-                let defination = match api_get_defination(&dict_val) {
-                    Ok(v) => Some(v),
-                    Err(_) => None,
-                };
-                let pinyin = match api_get_pinyin(&dict_val) {
-                    Ok(v) => Some(v),
-                    Err(_) => None,
-                };
-                let bopomofo = match api_get_bopomofo(&dict_val) {
-                    Ok(v) => Some(v),
-                    Err(_) => None,
-                };
-                moedict_item_result.push(MoedictItemResult {
-                    pinyin,
-                    bopomofo,
-                    defination,
-                });
-            }
-        }
-
-        moedict_item_result
-    }
-
-    pub fn get_english(&self) -> Option<String> {
-        match api_get_english(&self.json) {
-            Ok(v) => Some(v),
-            Err(_) => None,
-        }
     }
 }
--- src/formatter.rs
+++ src/formatter.rs
@@ -6,7 +6,7 @@ use opencc_rust::*;
 use owo_colors::OwoColorize;
 use tokio::runtime::Builder;
 
-use crate::api::{request_moedict, MoedictJson};
+use crate::api::{MoedictDefinition, MoedictRawResult, request_moedict};
 
 const LINE_LENGTH: usize = 80;
 
@@ -39,7 +39,7 @@ pub fn print_result(words: &[String], result_t2s: bool, translation_mode: bool) 
                     let result = if !translation_mode {
                         format_defination_output(&results[index])
                     } else {
-                        match &results[index].get_translations() {
+                        match &results[index].translation {
                             Some(translation) => format_translation_output(translation),
                             None => continue,
                         }
@@ -58,47 +58,49 @@ pub fn print_result(words: &[String], result_t2s: bool, translation_mode: bool) 
     })
 }
 
-fn format_defination_output(moedict_result: &MoedictJson) -> String {
+fn format_defination_output(moedict_result: &MoedictRawResult) -> String {
     let mut result = Vec::new();
-    if let Some(english) = moedict_result.get_english() {
+    if let Some(english) = moedict_result.english.to_owned() {
         result.push(
             format!("  英語：{}", english)
                 .fg_rgb::<125, 187, 222>()
                 .to_string(),
         );
     }
-    let definations = moedict_result.get_moedict_item_result_vec();
-    for i in definations {
-        if let Some(pinyin) = i.pinyin {
-            result.push(
-                format!("  拼音：{}", pinyin)
-                    .fg_rgb::<236, 184, 138>()
-                    .to_string(),
-            );
-        }
-        if let Some(bopomofo) = i.bopomofo {
-            result.push(
-                format!("  注音：{}", bopomofo)
-                    .fg_rgb::<208, 90, 110>()
-                    .to_string(),
-            );
-        }
-        if let Some(defination) = i.defination {
-            for (k, v) in defination {
-                if k != "notype" {
-                    result.push(format!("{:>3}：", k).fg_rgb::<168, 216, 165>().to_string());
-                }
-                for (index, value) in v.iter().enumerate() {
-                    let result_str = string_split_new_line(
-                        format!("{:>3}.{}", index + 1, value[0].to_string()),
-                        2,
-                    );
-                    result.push(result_str.fg_rgb::<129, 199, 212>().to_string());
-                    if !value[1..].is_empty() {
-                        for s in &value[1..] {
-                            let result_str =
-                                string_split_new_line(format!("    {}", s.to_string()), 4);
-                            result.push(result_str.fg_rgb::<220, 159, 180>().to_string());
+    if let Some(heteronyms) = &moedict_result.h {
+        for i in heteronyms {
+            if let Some(pinyin) = &i.p {
+                result.push(
+                    format!("  拼音：{}", pinyin)
+                        .fg_rgb::<236, 184, 138>()
+                        .to_string(),
+                );
+            }
+            if let Some(bopomofo) = &i.b {
+                result.push(
+                    format!("  注音：{}", bopomofo)
+                        .fg_rgb::<208, 90, 110>()
+                        .to_string(),
+                );
+            }
+            if let Some(definition) = &i.d {
+                let definition = definition_formatter(definition);
+                for (k, v) in definition {
+                    if k != "notype" {
+                        result.push(format!("{:>3}：", k).fg_rgb::<168, 216, 165>().to_string());
+                    }
+                    for (index, value) in v.iter().enumerate() {
+                        let result_str = string_split_new_line(
+                            format!("{:>3}.{}", index + 1, value[0].to_string()),
+                            2,
+                        );
+                        result.push(result_str.fg_rgb::<129, 199, 212>().to_string());
+                        if !value[1..].is_empty() {
+                            for s in &value[1..] {
+                                let result_str =
+                                    string_split_new_line(format!("    {}", s.to_string()), 4);
+                                result.push(result_str.fg_rgb::<220, 159, 180>().to_string());
+                            }
                         }
                     }
                 }
@@ -107,6 +109,45 @@ fn format_defination_output(moedict_result: &MoedictJson) -> String {
     }
 
     result.join("\n")
+}
+
+fn definition_formatter(definitions: &Vec<MoedictDefinition>) -> IndexMap<String, Vec<Vec<String>>> {
+    let mut result = IndexMap::new();
+    let mut count: usize = 0;
+    for i in definitions {
+        let t = if let Some(word_type) = i.word_type.to_owned() {
+            word_type
+        } else {
+            "notype".to_string()
+        };
+        if result.get(&t).is_none() {
+            result.insert(t.to_owned(), vec![Vec::new()]);
+            count = 0;
+        } else {
+            result.get_mut(&t).unwrap().push(Vec::new());
+        }
+        if let Some(f) = &i.f {
+            result.get_mut(&t).unwrap()[count].push(f.to_owned());
+        }
+        if let Some(q) = &i.q {
+            for i in q {
+                result.get_mut(&t).unwrap()[count].push(i.to_owned());
+            }
+        }
+        if let Some(e) = &i.e {
+            for i in e {
+                result.get_mut(&t).unwrap()[count].push(i.to_owned());
+            }
+        }
+        if let Some(l) = &i.l {
+            for i in l {
+                result.get_mut(&t).unwrap()[count].push(i.to_owned());
+            }
+        }
+        count += 1;
+    }
+
+    result
 }
 
 fn format_translation_output(translation: &IndexMap<String, Vec<String>>) -> String {
