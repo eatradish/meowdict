de18c2667fae46f83153f1d0caad9c4c380b2c43
eatradish <sakiiily@aosc.io>
2021-09-20 17:57:13 +0800

    meta: bump version for adapt to cargo-release

--- src/feat.rs
+++ src/feat.rs
@@ -84,7 +84,7 @@ pub fn search_word_to_json_result(
     words: Vec<String>,
     client: &Client,
     runtime: &Runtime,
-    result_t2s: bool
+    result_t2s: bool,
 ) -> Result<()> {
     let meowdict_results = get_dict_result(runtime, client, words)?;
     println!("{}", gen_dict_json_str(meowdict_results, result_t2s)?);
