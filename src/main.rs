f199e91737027da7498718f755d453506d268548
eatradish <sakiiily@aosc.io>
2021-09-25 14:16:13 +0800

    feat: add config file to save input-s2t / result-t2s ...

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,52 @@ pub mod console;
 mod feat;
 pub mod formatter;
 
+use std::{fs::{File, create_dir_all}, io::{Read, Write}, path::PathBuf};
+
 use crate::console::MeowdictConsole;
 use crate::feat::*;
 use anyhow::Result;
 use formatter::{opencc_convert, OpenccConvertMode};
 use tokio::runtime::Builder;
+use serde::{Deserialize, Serialize};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref CONFTG_PATH_DIRECTORY: PathBuf = dirs_next::config_dir().unwrap_or_else(|| PathBuf::from("./config"));
+    static ref CONFIG_PATH: PathBuf = CONFTG_PATH_DIRECTORY.join("meowdict.toml");
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct MeowdictConfig {
+    input_s2t: bool,
+    result_t2s: bool,
+    no_color: bool,
+}
 
 fn main() -> Result<()> {
+    create_dir_all(&*CONFTG_PATH_DIRECTORY)?;
+    let config = match File::open(&*CONFIG_PATH) {
+        Ok(mut f) => {
+            let mut buffer = Vec::new();
+            f.read_to_end(&mut buffer)?;
+            toml::from_slice(&buffer)?
+        },
+        Err(_) => {
+            let default_meowdict_config = MeowdictConfig {
+                input_s2t: false,
+                result_t2s: false,
+                no_color: false,
+            };
+            let mut f = File::create(&*CONFIG_PATH)?;
+            f.write_all(&toml::to_vec(&default_meowdict_config)?)?;
+
+            default_meowdict_config
+        }
+    };
     let app = cli::build_cli().get_matches();
     let translation_mode = app.is_present("translation");
     let jyutping_mode = app.is_present("jyutping");
-    let no_color_output = app.is_present("no-color-output");
+    let no_color_output = config.no_color || app.is_present("no-color-output");
     let json_mode = app.is_present("json");
     let client = reqwest::Client::new();
     let runtime = Builder::new_multi_thread()
@@ -23,8 +58,8 @@ fn main() -> Result<()> {
         .build()
         .unwrap();
     if let Some(words) = app.values_of("INPUT") {
-        let input_s2t = app.is_present("inputs2t");
-        let result_t2s = app.is_present("resultt2s");
+        let input_s2t = config.input_s2t || app.is_present("inputs2t");
+        let result_t2s = config.result_t2s || app.is_present("resultt2s");
         let mut words = words.into_iter().map(|x| x.into()).collect::<Vec<String>>();
         if input_s2t {
             words = words
@@ -48,8 +83,8 @@ fn main() -> Result<()> {
             search_word_to_dict_result(words, &client, &runtime, no_color_output, result_t2s)?;
         }
     } else {
-        let input_s2t_mode = app.is_present("inputs2tmode");
-        let result_t2s_mode = app.is_present("resultt2smode");
+        let input_s2t_mode =  config.input_s2t || app.is_present("inputs2tmode");
+        let result_t2s_mode = config.result_t2s || app.is_present("resultt2smode");
         let mut console = MeowdictConsole {
             input_s2t: input_s2t_mode,
             result_t2s: result_t2s_mode,
